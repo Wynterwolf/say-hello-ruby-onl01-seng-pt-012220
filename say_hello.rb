2405c48befd5ee0a101decfbec24cd5ba06b1366
Samantha Jordan <shinyguns@gmail.com>
2020-01-21 21:17:49 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,4 +1,9 @@
 # Build your say_hello method here
+<<<<<<< HEAD
 def say_hello(name = "Ruby Programmer")
   puts "Hello #{name}!"
+=======
+def say_hello(name = "Ruby Programmer!")
+  put "Hello #{name}!"
+>>>>>>> 820b17a849a88d79fd91c93aec0f0fef478b24ad
 end
